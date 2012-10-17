7cb677ba0d21e591f739e1e961dfeda46ffcb5eb
Vaclav Klecanda <vencax77@gmail.com>
2012-10-17 18:45:37 +0200

    settings module

---
+++ elephantblog/fcms_settings.py
@@ -0,0 +1,27 @@
+'''
+Created on Oct 16, 2012
+
+@author: vencax
+'''
+
+def elephantblog_entry_url_app(self):
+    from feincms.content.application.models import app_reverse
+    return app_reverse('elephantblog_entry_detail', 'elephantblog.urls', kwargs={
+        'year': self.published_on.strftime('%Y'),
+        'month': self.published_on.strftime('%m'),
+        'day': self.published_on.strftime('%d'),
+        'slug': self.slug,
+        })
+
+def elephantblog_categorytranslation_url_app(self):
+    from feincms.content.application.models import app_reverse
+    return app_reverse('elephantblog_category_detail', 'elephantblog.urls', kwargs={
+        'slug': self.slug,
+        })
+
+url_overrides = {
+    'elephantblog.entry': elephantblog_entry_url_app,
+    'elephantblog.categorytranslation': elephantblog_categorytranslation_url_app,
+}
+
+app_content = ('elephantblog.urls', 'Blog')
